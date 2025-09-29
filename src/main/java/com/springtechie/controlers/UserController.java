feb3c593fa09fb23f8e958b215c0e15f3e6b698d
springtechie <springframeworkdeveloper@gmail.com>
2025-09-29 06:37:07 +0530

    Added new code

--- src/main/java/com/springtechie/controlers/UserController.java
+++ src/main/java/com/springtechie/controlers/UserController.java
@@ -3,7 +3,6 @@ package com.springtechie.controlers;
 import com.springtechie.models.User;
 import com.springtechie.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -13,10 +12,6 @@ public class UserController {
 
     @Autowired
     private UserService userService;
-
-    @Value("${env}")
-    private String env;
-
 
     @GetMapping("/get/user/id/{id}")
     public User fetchUser(@PathVariable int id) {
